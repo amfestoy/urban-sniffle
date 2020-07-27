e630387cc8827aa6e4976baf107f538aa0ee6d2d
Andrea Marie Festøy <andrea@gojust.com>
2020-07-27 15:01:38 +0200

    Get all product titles for one category with no edge case

--- src/scrape.js
+++ src/scrape.js
@@ -1,19 +1,58 @@
 const request = require("request");
 const cheerio = require("cheerio");
+
+const getKolonialLink = (href) => {
+  return "https://kolonial.no" + href;
+};
 request("https://kolonial.no/produkter", (error, response, html) => {
   if (!error && response.statusCode === 200) {
     const $ = cheerio.load(html);
 
     const kolonialSidebar = $(".filter-sidebar a");
 
-    const productCategories = [];
+    const mainProductCategories = [];
 
     kolonialSidebar.each((i, el) => {
-      const href = $(el).attr("href");
+      const href = getKolonialLink($(el).attr("href"));
       const titel = $(el).text().replace(/Ny!/, "").trim();
-      productCategories.push({ titel, href });
+      mainProductCategories.push({ titel, href });
     });
 
-    console.log(productCategories);
+    const fruktOgBær = mainProductCategories[3];
+    console.log(fruktOgBær);
+
+    request(fruktOgBær.href, (error, response, html) => {
+      if (!error && response.statusCode === 200) {
+        const $ = cheerio.load(html);
+
+        const navPills = $(".nav.nav-pills a");
+
+        const subProductCategories = [];
+
+        navPills.each((i, el) => {
+          const titel = $(el).text().trim();
+          const href = getKolonialLink($(el).attr("href"));
+
+          subProductCategories.push({ titel, href });
+        });
+
+        console.log(subProductCategories);
+
+        const frukt = subProductCategories[0];
+        console.log(frukt);
+        request(frukt.href, (error, response, html) => {
+          const $ = cheerio.load(html);
+
+          const productCategoryList = $(".product-category-list.ws-xs a");
+
+          productCategoryList.each((i, el) => {
+            const title = $(el).find("h3").children().first().text();
+            console.log(title);
+          });
+        });
+      }
+    });
+
+    //console.log(mainProductCategories);
   }
 });

cbebbfcb72f9f2ad488ddf3cdb6fe987412b074c
Andrea Marie Festøy <andrea@gojust.com>
2020-07-27 20:42:12 +0200

    Get all products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./App.css";
 import { PRODUCT_CATEGORIES } from "./productData.js";
-
+import getAllProducts from "./scrape.js";
 function App() {
+  const products = getAllProducts();
+  console.log(products);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Kolonial produktkatalog</h1>
+        <ul>
+          {PRODUCT_CATEGORIES.map((category) => {
+            const href = "https://kolonial.no" + category.href;
+            return (
+              <li>
+                <a href={href}>{category.titel}</a>;
+              </li>
+            );
+          })}
+        </ul>
       </header>
     </div>
   );
--- src/scrape.js
+++ src/scrape.js
@@ -1,11 +1,49 @@
-const request = require("request");
 const cheerio = require("cheerio");
+const fetch = require("node-fetch");
 
 const getKolonialLink = (href) => {
   return "https://kolonial.no" + href;
 };
-request("https://kolonial.no/produkter", (error, response, html) => {
-  if (!error && response.statusCode === 200) {
+
+const getSubCategories = async (e) => {
+  const response = await fetch(e.link);
+  const html = await response.text();
+  const $ = cheerio.load(html);
+
+  const navPills = $(".nav.nav-pills a");
+  const subCategories = [];
+
+  navPills.each((i, el) => {
+    const title = $(el).text().trim();
+    const link = getKolonialLink($(el).attr("href"));
+
+    subCategories.push({ title, link });
+  });
+
+  return { ...e, subCategories };
+};
+
+const getProducts = async (e) => {
+  const response = await fetch(e.link);
+  const html = await response.text();
+
+  const $ = cheerio.load(html);
+
+  const productCategoryList = $(".product-category-list.ws-xs a");
+
+  const products = [];
+  productCategoryList.each((i, el) => {
+    const title = $(el).find("h3").children().first().text().trim();
+    const price = $(el).find(".price.label.label-price").text().trim();
+    products.push({ title, price });
+  });
+
+  return { ...e, products };
+};
+
+const mainProductCategories = fetch("https://kolonial.no/produkter")
+  .then((res) => res.text())
+  .then((html) => {
     const $ = cheerio.load(html);
 
     const kolonialSidebar = $(".filter-sidebar a");
@@ -13,46 +51,56 @@ request("https://kolonial.no/produkter", (error, response, html) => {
     const mainProductCategories = [];
 
     kolonialSidebar.each((i, el) => {
-      const href = getKolonialLink($(el).attr("href"));
-      const titel = $(el).text().replace(/Ny!/, "").trim();
-      mainProductCategories.push({ titel, href });
+      const link = getKolonialLink($(el).attr("href"));
+      const title = $(el).text().replace(/Ny!/, "").trim();
+      mainProductCategories.push({ title, link });
     });
 
-    const fruktOgBær = mainProductCategories[3];
-    console.log(fruktOgBær);
+    return mainProductCategories;
+  });
 
-    request(fruktOgBær.href, (error, response, html) => {
-      if (!error && response.statusCode === 200) {
-        const $ = cheerio.load(html);
-
-        const navPills = $(".nav.nav-pills a");
-
-        const subProductCategories = [];
-
-        navPills.each((i, el) => {
-          const titel = $(el).text().trim();
-          const href = getKolonialLink($(el).attr("href"));
-
-          subProductCategories.push({ titel, href });
-        });
-
-        console.log(subProductCategories);
-
-        const frukt = subProductCategories[0];
-        console.log(frukt);
-        request(frukt.href, (error, response, html) => {
-          const $ = cheerio.load(html);
-
-          const productCategoryList = $(".product-category-list.ws-xs a");
-
-          productCategoryList.each((i, el) => {
-            const title = $(el).find("h3").children().first().text();
-            console.log(title);
-          });
-        });
-      }
-    });
-
-    //console.log(mainProductCategories);
+const mainProductCategoriesWithSubCategories = mainProductCategories.then(
+  async (cat) => {
+    return Promise.all(cat.map((e) => getSubCategories(e)));
   }
-});
+);
+
+mainProductCategoriesWithSubCategories
+  .then((cat) => {
+    console.log("duklartedet", cat[3]);
+    return Promise.all(cat[3].subCategories.map((e) => getProducts(e)));
+  })
+  .then((res) => console.log(res[0]));
+
+const getAllProducts = async () => {
+  const mainProductCategories = fetch("https://kolonial.no/produkter")
+    .then((res) => res.text())
+    .then((html) => {
+      const $ = cheerio.load(html);
+
+      const kolonialSidebar = $(".filter-sidebar a");
+
+      const mainProductCategories = [];
+
+      kolonialSidebar.each((i, el) => {
+        const link = getKolonialLink($(el).attr("href"));
+        const title = $(el).text().replace(/Ny!/, "").trim();
+        mainProductCategories.push({ title, link });
+      });
+
+      return mainProductCategories;
+    });
+
+  const mainProductCategoriesWithSubCategories = mainProductCategories.then(
+    async (cat) => {
+      return Promise.all(cat.map((e) => getSubCategories(e)));
+    }
+  );
+
+  return mainProductCategoriesWithSubCategories.then((cat) => {
+    console.log("duklartedet", cat[3]);
+    return Promise.all(cat[3].subCategories.map((e) => getProducts(e)));
+  });
+};
+
+export default getAllProducts;

9d4bbb9507d38067e6258641c884d52cdcdecfc7
Andrea Marie Festøy <andrea@gojust.com>
2020-07-27 21:31:57 +0200

    Render all products in browser

--- src/App.js
+++ src/App.js
@@ -1,28 +1,112 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { PRODUCT_CATEGORIES } from "./productData.js";
-import getAllProducts from "./scrape.js";
-function App() {
-  const products = getAllProducts();
-  console.log(products);
+import { getMainCategories, getSubCategories, getProducts } from "./scrape.js";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [mainCategories, setMainCategories] = useState([]);
+  const [subCategories, setSubCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const [selectedCategory, setSelectedCategory] = useState();
+  const [selectedSubCategory, setSelectedSubCategory] = useState();
+
+  // get all categories from kolonial on load
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const categories = await getMainCategories();
+      setMainCategories(categories);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  // get all sub categories when a category is selected
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const subCategories = await getSubCategories(selectedCategory);
+
+      setSubCategories(subCategories);
+      setIsLoading(false);
+    };
+
+    // when selecting a new category sub categories and products are removed
+    setSelectedSubCategory();
+    setSubCategories([]);
+    setProducts([]);
+    if (!!selectedCategory) {
+      fetchData();
+    }
+  }, [selectedCategory]);
+
+  // get all products when a sub category is selected
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const products = await getProducts(selectedSubCategory);
+
+      setProducts(products);
+      setIsLoading(false);
+    };
+
+    // when selecting a sub category all products are removed
+    setProducts([]);
+    if (!!selectedCategory) {
+      fetchData();
+    }
+  }, [selectedSubCategory]);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Kolonial produktkatalog</h1>
-        <ul>
-          {PRODUCT_CATEGORIES.map((category) => {
-            const href = "https://kolonial.no" + category.href;
-            return (
-              <li>
-                <a href={href}>{category.titel}</a>;
-              </li>
-            );
-          })}
-        </ul>
+        {mainCategories && (
+          <ul>
+            {mainCategories.map((category) => {
+              const onClick = () => {
+                setSelectedCategory(category);
+              };
+
+              return (
+                <li>
+                  <b onClick={onClick}>{category.title}</b>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        {subCategories && (
+          <ul>
+            {subCategories.map((category) => {
+              const onClick = () => setSelectedSubCategory(category);
+              return (
+                <li>
+                  <b onClick={onClick}>{category.title}</b>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        {products && (
+          <ul>
+            {products.map((product) => {
+              return (
+                <li>
+                  {product.title}
+                  {product.price}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        {isLoading && (
+          <div>Vent et øyeblikk mens vi henter kolonial sine deilige varer</div>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/scrape.js
+++ src/scrape.js
@@ -5,75 +5,8 @@ const getKolonialLink = (href) => {
   return "https://kolonial.no" + href;
 };
 
-const getSubCategories = async (e) => {
-  const response = await fetch(e.link);
-  const html = await response.text();
-  const $ = cheerio.load(html);
-
-  const navPills = $(".nav.nav-pills a");
-  const subCategories = [];
-
-  navPills.each((i, el) => {
-    const title = $(el).text().trim();
-    const link = getKolonialLink($(el).attr("href"));
-
-    subCategories.push({ title, link });
-  });
-
-  return { ...e, subCategories };
-};
-
-const getProducts = async (e) => {
-  const response = await fetch(e.link);
-  const html = await response.text();
-
-  const $ = cheerio.load(html);
-
-  const productCategoryList = $(".product-category-list.ws-xs a");
-
-  const products = [];
-  productCategoryList.each((i, el) => {
-    const title = $(el).find("h3").children().first().text().trim();
-    const price = $(el).find(".price.label.label-price").text().trim();
-    products.push({ title, price });
-  });
-
-  return { ...e, products };
-};
-
-const mainProductCategories = fetch("https://kolonial.no/produkter")
-  .then((res) => res.text())
-  .then((html) => {
-    const $ = cheerio.load(html);
-
-    const kolonialSidebar = $(".filter-sidebar a");
-
-    const mainProductCategories = [];
-
-    kolonialSidebar.each((i, el) => {
-      const link = getKolonialLink($(el).attr("href"));
-      const title = $(el).text().replace(/Ny!/, "").trim();
-      mainProductCategories.push({ title, link });
-    });
-
-    return mainProductCategories;
-  });
-
-const mainProductCategoriesWithSubCategories = mainProductCategories.then(
-  async (cat) => {
-    return Promise.all(cat.map((e) => getSubCategories(e)));
-  }
-);
-
-mainProductCategoriesWithSubCategories
-  .then((cat) => {
-    console.log("duklartedet", cat[3]);
-    return Promise.all(cat[3].subCategories.map((e) => getProducts(e)));
-  })
-  .then((res) => console.log(res[0]));
-
-const getAllProducts = async () => {
-  const mainProductCategories = fetch("https://kolonial.no/produkter")
+export const getMainCategories = async () => {
+  const mainCategories = fetch("https://kolonial.no/produkter")
     .then((res) => res.text())
     .then((html) => {
       const $ = cheerio.load(html);
@@ -91,16 +24,41 @@ const getAllProducts = async () => {
       return mainProductCategories;
     });
 
-  const mainProductCategoriesWithSubCategories = mainProductCategories.then(
-    async (cat) => {
-      return Promise.all(cat.map((e) => getSubCategories(e)));
-    }
-  );
-
-  return mainProductCategoriesWithSubCategories.then((cat) => {
-    console.log("duklartedet", cat[3]);
-    return Promise.all(cat[3].subCategories.map((e) => getProducts(e)));
-  });
+  return mainCategories;
 };
 
-export default getAllProducts;
+export const getSubCategories = async (e) => {
+  const response = await fetch(e.link);
+  const html = await response.text();
+  const $ = cheerio.load(html);
+
+  const navPills = $(".nav.nav-pills a");
+  const subCategories = [];
+
+  navPills.each((i, el) => {
+    const title = $(el).text().trim();
+    const link = getKolonialLink($(el).attr("href"));
+
+    subCategories.push({ title, link });
+  });
+
+  return subCategories;
+};
+
+export const getProducts = async (e) => {
+  const response = await fetch(e.link);
+  const html = await response.text();
+
+  const $ = cheerio.load(html);
+
+  const productCategoryList = $(".product-category-list.ws-xs a");
+
+  const products = [];
+  productCategoryList.each((i, el) => {
+    const title = $(el).find("h3").children().first().text().trim();
+    const price = $(el).find(".price.label.label-price").text().trim();
+    products.push({ title, price });
+  });
+
+  return products;
+};
